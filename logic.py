3397bbb6c84f4fe42be10f4c7900253d38168ad7
allannar <alannar3651@gmail.com>
2025-10-23 16:13:20 +0300

    logic.py

---
+++ logic.py
@@ -0,0 +1,30 @@
+import time, threading, schedule
+from telebot import TeleBot
+
+bot = TeleBot('8424301291:AAF_NtCw61Ux91OBfW4SMsGxOkzKlZEVi2I')
+def timer():
+    def beep(chat_id) -> None:
+        """Send the beep message."""
+        bot.send_message(chat_id, text='Beep!')
+
+
+    @bot.message_handler(commands=['set'])
+    def set_timer(message):
+        args = message.text.split()
+        if len(args) > 1 and args[1].isdigit():
+            sec = int(args[1])
+            schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+        else:
+            bot.reply_to(message, 'Используй: /set <seconds>')
+
+
+    @bot.message_handler(commands=['unset'])
+    def unset_timer(message):
+        schedule.clear(message.chat.id)
+
+
+    if __name__ == '__main__':
+        threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
