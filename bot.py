344364d83cde4a06477d2f9c8f24d00a84bc218b
allannar <alannar3651@gmail.com>
2025-10-23 15:55:36 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import telebot
+import bot
+
+bot = telebot.TeleBot('YOUR TOKEN HERE')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот от allannar. Сейчас он в стадии разработки, но это временно... A little bit of waiting ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['help'])
+def send_help(massage):
+    bot.reply_to(massage, 'Тут скоро появиться список команд бота, A little bit of waiting')
+
+@bot.message_handler(commands=['info'])
+def send_info(massage):
+    bot.reply_to(massage, 'Тут скоро появиться информация о боте, A little bit of waiting')
+
+#@bot.message_handler(commands=['generator'])
+#def send_password(massage): ( В ПРОЦЕССЕ ) 
+    
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
